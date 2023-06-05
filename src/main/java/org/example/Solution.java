63050cab37a2a06f9832c44934f2b0b0ef80ca71
alek <wait82985@gmail.com>
2023-06-05 17:06:55 +0600

    algo works but 34/39 TLE

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
         int n = arr.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-
+        int globalMax = 1;
         for (int i = 1; i < n; i++) {
             int localMax = 1;
             for (int j = 0; j < i; j++) {
@@ -16,8 +16,9 @@ public class Solution {
                 }
             }
             dp[i] = localMax;
+            globalMax = Math.max(globalMax, localMax);
         }
 
-        return dp[n - 1];
+        return globalMax;
     }
 }
